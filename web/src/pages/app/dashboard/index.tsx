4d18bcfbb8c98f51ceec46cbe0bb5880141efe52
Partha <partha@Parthas-MacBook-Pro.local>
2024-04-11 01:39:25 +0530

    test suite issue

--- web/src/pages/app/dashboard/index.tsx
+++ web/src/pages/app/dashboard/index.tsx
@@ -3,7 +3,7 @@ import { ExecutionHistory } from "../history/historys";
 
 export function AppDashboard() {
   return (
-    <div className="mb-4 py-4 grid grid-cols-1 gap-6 xl:grid-cols-3">
+    <div className="mb-4 py-4 grid grid-cols-1 gap-6 xl:grid-cols-2">
       <div className="col-span-2">
         <ExecutionHistory></ExecutionHistory>{" "}
       </div>
